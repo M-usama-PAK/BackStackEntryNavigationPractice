d38c9aef6fc73a63404474393c9f4b7d60ce38b5
M-usama-PAK <musamaims@gmail.com>
2022-12-23 18:41:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/backstackentrynavigationpractice/SecondFragment.kt
@@ -0,0 +1,81 @@
+package com.example.backstackentrynavigationpractice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.backstackentrynavigationpractice.databinding.FragmentSecondBinding
+import com.google.android.material.snackbar.Snackbar
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    lateinit var binding: FragmentSecondBinding
+    private val args: SecondFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentSecondBinding.inflate(inflater)
+        // Inflate the layout for this fragment
+        parentFragment?.view?.let { Snackbar.make(it,"the argument sent from firstfragment : ${args.dummyData}",Snackbar.LENGTH_SHORT).show() }
+
+        // Here is the code for backstack entry
+//        findNavController().run {
+//            previousBackStackEntry?.savedStateHandle?.set("backStackData","Hello back stack data")
+//            navigateUp()
+//        }
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("backStackData","hello back stack data")
+
+
+
+        ///
+
+        return binding.root
+
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SecondFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SecondFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
